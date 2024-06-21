940c6ed996c4f81975ee397cc581afc420786811
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-21 03:00:14 -0700

    Print Tebehpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: 0 when successful
+ *
+ */
+
+int main(void)
+{
+	char c;
+	
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
