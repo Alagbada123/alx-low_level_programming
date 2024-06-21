2cc7e101a041b603c1d57e08ecdf080eef4bdd94
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-21 02:52:59 -0700

    Pring Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: 0 when successful
+ *
+ */
+
+int main(void)
+{
+	char num;
+	
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
