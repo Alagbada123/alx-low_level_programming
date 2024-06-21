06a1a12ae188c6b0fe2df12c49d2327eafeff7e5
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-21 02:49:06 -0700

    Print Alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: 0 when succesful
+ *
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
