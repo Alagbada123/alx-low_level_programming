863bff1ef9317e701597cbf4373f3901df1c8e8d
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-21 13:34:31 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,12 +11,10 @@
 int main(void)
 {
 	int n;
-	int last_digit;
-	
+	int last_digit;	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
-       
 	printf("Last digit of %d is %d ", n, last_digit);
 	if (last_digit > 5)
 	{
@@ -30,6 +28,5 @@ int main(void)
 	{
 		printf("and is less than 6 and not 0\n");
 	}
-	
 	return (0);
 }
