cfc68297c8bed946a44c6ae9d54488f74f625595
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-21 15:24:18 -0700

    Print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,13 @@
 int main(void)
 {
 	int i, j;
-	
+
 	for (i = 0; i <= 8; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			
 			if (i != 8 || j != 9)
 			{
 				putchar(',');
@@ -25,8 +24,6 @@ int main(void)
 			}
 		}
 	}
-	
 	putchar('\n');
-	
 	return (0);
 }
