9cf258af54920760fbefefcd584e44124982773f
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-21 03:41:49 -0700

    Print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: 0 when successful
+ *
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 98; i++)
+	{
+		int first_digit_i = i / 10;
+		int second_digit_i = i % 10;
+		
+		for (j = i + 1; j <= 99; j++)
+		{
+			int first_digit_j = j / 10;
+			int second_digit_j = j % 10;
+			
+			putchar(first_digit_i + '0');
+			putchar(second_digit_i + '0');
+			putchar(' ');
+			putchar(first_digit_j + '0');
+			putchar(second_digit_j + '0');
+			
+			if (!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	
+	putchar('\n');
+	
+	return 0;
+}
