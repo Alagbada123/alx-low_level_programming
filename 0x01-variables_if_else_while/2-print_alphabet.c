ed26592e12fa36af5a237494f64c206ade36fa25
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-21 02:36:20 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: 0 when successful
+ *
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
