55b576499443ecd01ee01a557dae4db6eca522b4
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-25 15:39:12 -0700

    This is another file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned long fib1 = 1, fib2 = 2, next_fib = 0;
+	unsigned long sum_even = 0;
+
+	while (fib1 <= 4000000)
+	{
+		if (fib1 % 2 == 0)
+		{
+			sum_even += fib1;
+		}
+		next_fib = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = next_fib;
+	}
+	printf("%lu\n", sum_even);
+	return (0);
+}
