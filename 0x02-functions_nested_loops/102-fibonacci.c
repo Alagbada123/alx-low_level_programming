b786c327e8084717e9ea3c00e16f501cb76598be
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-25 16:54:16 -0700

    This is another file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,9 +4,8 @@
  * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
  *        separated by a comma followed by a space.
  *
- * Return: 0 Always
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	int count;
