5b7b0fdd09b6db375ddc7f1388565cbb93368397
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-25 15:31:35 -0700

    This is another file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int count;
+
+	unsigned long fib1 = 1, fib2 = 2, next_fib;printf("%lu, %lu", fib1, fib2);
+	
+	for (count = 3; count <= 50; count++)
+	{
+		next_fib = fib1 + fib2;
+		printf(", %lu", next_fib);
+		fib1 = fib2;
+		fib2 = next_fib;
+	}
+	printf("\n");
+	return (0);
+}
