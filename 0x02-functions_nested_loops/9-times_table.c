48c7a09be632efcc82141b2a59c53d7400d0ef2d
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-25 13:17:24 -0700

    This is the next file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,17 +21,15 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (result < 10)
-					_putchar(' ');
 			}
 			if (result >= 10)
 			{
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
+				_putchar(result + '0');
 			}
 			else
 			{
-				_putchar(result + '0');
+				_putchar(' ');
+				_putchar(result % 10 + '0');
 			}
 		}
 		_putchar('\n');
