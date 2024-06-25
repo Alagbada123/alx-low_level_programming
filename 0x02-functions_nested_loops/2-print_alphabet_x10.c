fb3080604f304bfe8d942e156e3196879af3d85f
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-25 09:58:59 -0700

    This is the third work

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "holberton.h"
+
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+}
