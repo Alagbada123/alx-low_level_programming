4da47f27bc194247f15c4b0bbba35192fdb08b5e
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-25 13:21:36 -0700

    This is the next file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,34 +4,38 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
+
+
 void times_table(void)
 {
-	int i, j, result;
+    int i, j, result;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			result = i * j;
-			if (j == 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (result >= 10)
-			{
-				_putchar(result + '0');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(result % 10 + '0');
-			}
-		}
-		_putchar('\n');
-	}
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            result = i * j;
+
+            /* Print the result with proper formatting */
+            if (j == 0)
+            {
+                _putchar('0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+
+                /* Print tens digit if greater than 0 */
+                if (result >= 10)
+                    _putchar(result / 10 + '0');
+                else
+                    _putchar(' ');
+
+                /* Print units digit */
+                _putchar(result % 10 + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
