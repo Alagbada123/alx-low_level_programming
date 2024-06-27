5f004a5210c28064d5b16630aa375b93f851aedb
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-27 10:32:11 -0700

    This is another one

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int _putchar(char c);
 void more_numbers(void);
 int _putchar(char c);
 void print_line(int n);
+int _putchar(char c);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
