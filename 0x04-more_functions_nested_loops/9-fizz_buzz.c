aac51bf75a14edd01e8d5c9fd56e5fc328d931b5
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-06-27 15:04:33 -0700

    This is another one

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - main block, solve fizz buzz from numbers 1 to 100
+ * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
+ * Multiples of both 3 and 5 should print FizzBuzz.
+ * Return: 0
+ */
+
 int main(void)
 {
 	for (int i = 1; i <= 100; i++)
@@ -20,7 +27,7 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-		if (i < 100)
+		if (i != 100)
 		{
 			printf(" ");
 		}
