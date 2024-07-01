13ea98af9213adeaa798901974d976454a87de7f
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-07-01 14:24:06 -0700

    This i sthe fourth one

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -14,15 +14,3 @@ void _puts(char *str)
 	}
 	_putchar('\n');
 }
-
-/**
- * _putchar - writes a character to stdout
- * @c: the character to write
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
