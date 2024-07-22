bc8de8c5c61f519f9de153630c8d8dab4ac6f718
Abiodun Alagbada <abiodunalagbada@gmail.com>
2024-07-22 14:41:56 -0700

    This is another one

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - This is the main function
+ * brief Entry point of the program.
+ *
+ * This function prints the name of the file it was compiled from,
+ * followed by a new line.
+ *
+ *  Return: 0 upon successful completion.
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
